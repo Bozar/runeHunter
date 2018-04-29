3b3d52723ecb7c2d3089c44cb9018b6bcba47f65
Bozar <5583771+Bozar@users.noreply.github.com>
2018-04-29 14:30:33 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# runeHunter
-A coffee break roguelike game written in JS. Explore the dungeon, collect 3 runes and escape alive.
+# Rune Hunter
+
+Rune Hunter is a coffe break Roguelike game written in Javascript with [rot.js](https://github.com/ondras/rot.js). Explore the dungeon, stay away from ghosts, and collect 3 runes as quickly as possible.
+
---
+++ scripts/game.js
@@ -0,0 +1,130 @@
+'use strict'
+
+// ----- Version number, development switch, seed & color +++++
+var Game = {}
+Game._version = '0.0.1-dev'
+Game._develop = true
+Game.getVersion = function () { return this._version }
+Game.getDevelop = function () { return this._develop }
+Game.setDevelop = function () { this._develop = !this._develop }
+
+// set seed manually for testing, '#' can be omitted
+// there are no hyphens ('-') inside numbered seed
+// example:
+// Game._devSeed = '#12345'
+Game.getDevSeed = function () { return this._devSeed }
+
+Game._color = new Map()
+Game._color.set(null, '')
+Game._color.set('white', '#ABB2BF')
+Game._color.set('black', '#262626')
+Game._color.set('grey', '#666666')
+Game._color.set('orange', '#FF9900')
+
+Game.getColor = function (color) { return Game._color.get(color) }
+
+// ----- Key-bindings +++++
+Game.input = {}
+Game.input.keybind = new Map()
+// [mode1: [keybind1], mode2: [keybind2], ...]
+// keybind1 -> [action1: [key1_1, key1_2, ...],
+//              action2: [key2_1, key2_2, ...], ...]
+
+// keys that cannot be remapped by player
+Game.input.keybind.set('fixed', new Map())
+Game.input.keybind.get('fixed').set('space', [' '])
+Game.input.keybind.get('fixed').set('enter', ['Enter'])
+Game.input.keybind.get('fixed').set('esc', ['Escape'])
+
+// movement
+Game.input.keybind.set('move', new Map())
+Game.input.keybind.get('move').set('left', ['h', 'ArrowLeft'])
+Game.input.keybind.get('move').set('down', ['j', 'ArrowDown'])
+Game.input.keybind.get('move').set('up', ['k', 'ArrowUp'])
+Game.input.keybind.get('move').set('right', ['l', 'ArrowRight'])
+
+// drop treasure
+Game.input.keybind.set('drop', new Map())
+Game.input.keybind.get('drop').set('skull', ['s'])
+Game.input.keybind.get('drop').set('coin', ['c'])
+Game.input.keybind.get('drop').set('gem', ['g'])
+
+Game.input.getAction = function (keyInput, mode) {
+  if (!mode) {
+    Game.getDevelop() && console.log('Undefined mode.')
+    return null
+  }
+
+  for (const [key, value] of Game.input.keybind.get(mode)) {
+    if (value.indexOf(keyInput.key) > -1) {
+      return key
+    }
+  }
+  return null
+}
+
+Game.input.listenEvent = function (event, handler) {
+  handler = Game.screens[String(handler)]
+    ? Game.screens[handler].keyInput
+    : handler
+
+  switch (event) {
+    case 'add':
+      window.addEventListener('keydown', handler)
+      break
+    case 'remove':
+      window.removeEventListener('keydown', handler)
+      break
+  }
+}
+
+// ----- The position & size of screen elements +++++
+Game.UI = function (width, height) {
+  this._width = width || null
+  this._height = height || null
+
+  this._x = null
+  this._y = null
+}
+
+Game.UI.prototype.getWidth = function () { return this._width }
+Game.UI.prototype.getHeight = function () { return this._height }
+Game.UI.prototype.getX = function () { return this._x }
+Game.UI.prototype.getY = function () { return this._y }
+
+Game.UI.canvas = new Game.UI(70, 26)
+
+Game.display = new ROT.Display({
+  width: Game.UI.canvas.getWidth(),
+  height: Game.UI.canvas.getHeight(),
+  fg: Game.getColor('white'),
+  bg: Game.getColor('black'),
+  fontSize: 20,
+  fontFamily: (function () {
+    let family = 'dejavu sans mono'
+    family += ', consolas'
+    family += ', monospace'
+
+    return family
+  }())
+})
+
+// ``` The main screen +++
+Game.UI.padTopBottom = 0.5
+Game.UI.padLeftRight = 1
+Game.UI.padModeStatus = 1
+Game.UI.padModeMessage = 0
+Game.UI.padMessageDungeon = 1
+
+// ----- Initialization +++++
+window.onload = function () {
+  if (!ROT.isSupported()) {
+    window.alert(Game.text.error('browser'))
+    return
+  }
+  document.getElementById('game').appendChild(Game.display.getContainer())
+
+  Game.display.drawText(1, 1, 'hello world')
+  // Game.display.clear()
+  // Game.screens.main.enter()
+}
